7c10762924420017efdfca9991459f4198b071e5
Jonathan Sapp <jonathansapp@jonathans-mbp.lan>
2021-07-09 21:41:49 -0400

    Adding sequence for game

---
+++ client/secret.js
@@ -0,0 +1,26 @@
+// Start game
+
+// 1. Assign roles
+//     1. Let everyone know who the others are
+// 2. Pick a chancellor
+// 3. Vote yes or no
+//     1. If majority yes then pass
+//     2. Else break and go back to step one with new President
+// 4. If 3 fascist cards down, ask it chancellor is hitler
+//     1. If yes end game
+//     2. Else pass
+// 5. President draws thee cards
+// 6. President discards one card
+// 7. Chancellor takes the two leftover cards
+// 8. If 5 fascist cards chancellor can request veto
+//     1. If president agrees, break
+// 9. Chancellor plays one card
+// 10. Did the liberal board get full?
+//     1. Yes end game
+//     2. No continue
+// 11. Did fascist board get full?
+//     1. Yes end game
+//     2. No check for executive power
+//     3. Enact executive power
+// 12. Check cards
+//     1. If fewer than 3, shuffle everything
